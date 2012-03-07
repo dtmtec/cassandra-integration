3e3616f969f1ad74ea9851034607ded4f90cb07c
Marcelo Murad <marcelo.murad@dtmconsultoria.com>
2012-03-06 23:07:12 -0300

    Refactoring base spec to use let.

--- spec/base_spec.rb
+++ spec/base_spec.rb
@@ -3,18 +3,16 @@ require 'dummy_class'
 
 describe CassandraIntegration::Base do
 
-  before(:each) do
-    @dummy = DummyClass.new
-  end
+  let(:dummy) {DummyClass.new}
 
   it 'should call my callback method when create is called' do
-    @dummy.should_receive(:replicate)
-    @dummy.save
+    dummy.should_receive(:replicate)
+    dummy.save
   end
 
   it 'should call my callback method when validation is called' do
-    @dummy.should_receive(:set_cassandra_sync_identifier)
-    @dummy.valid?
+    dummy.should_receive(:set_cassandra_sync_identifier)
+    dummy.valid?
   end
 
   it 'should permit alter cassandra column family name' do
@@ -25,15 +23,15 @@ describe CassandraIntegration::Base do
   describe '#replicate' do
 
     it 'should create a Proxy passing self as argument' do
-      CassandraIntegration::Proxy.should_receive(:new).with(@dummy).and_return(mock(:sync => true))
-      @dummy.replicate
+      CassandraIntegration::Proxy.should_receive(:new).with(dummy).and_return(mock(:sync => true))
+      dummy.replicate
     end
 
     it 'should call Proxy sync method' do
       proxy = mock(:proxy)
       proxy.should_receive(:sync)
       CassandraIntegration::Proxy.stub!(:new).and_return(proxy)
-      @dummy.replicate
+      dummy.replicate
     end
 
   end
@@ -41,7 +39,7 @@ describe CassandraIntegration::Base do
   describe '#to_cassandra_sync_identifier' do
 
     it "should raise 'not implemented'" do
-      expect {@dummy.to_cassandra_sync_identifier}.to raise_error('Method to_cassandra_sync_identifier is not implemented!')
+      expect {dummy.to_cassandra_sync_identifier}.to raise_error('Method to_cassandra_sync_identifier is not implemented!')
     end
 
   end
@@ -49,15 +47,15 @@ describe CassandraIntegration::Base do
   describe '#set_cassandra_sync_identifier' do
 
     it "should raise exception if cassandra_sync_identifier was not created" do
-      expect {@dummy.set_cassandra_sync_identifier}.to raise_error('Your model does not have cassandra_sync_identifier column.')
+      expect {dummy.set_cassandra_sync_identifier}.to raise_error('Your model does not have cassandra_sync_identifier column.')
     end
 
     it "should verify if cassandra_sync_identifier was created" do
       class DummyClass
         attr_accessor :cassandra_sync_identifier
       end
-      @dummy.stub!(:to_cassandra_sync_identifier).and_return('to_cassandra_sync_identifier')
-      @dummy.set_cassandra_sync_identifier.should eq('to_cassandra_sync_identifier')
+      dummy.stub!(:to_cassandra_sync_identifier).and_return('to_cassandra_sync_identifier')
+      dummy.set_cassandra_sync_identifier.should eq('to_cassandra_sync_identifier')
     end
 
   end
